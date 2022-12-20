c5b0c68b65c3c37bc6655b1093086c4a2c67227c
Donghoon Kim <magenta1223@naver.com>
2022-12-21 01:17:53 +0900

    update backend url

--- config/front/dist/js/546.92d3d860.js
+++
@@ -1,8 +0,0 @@
-"use strict";(self["webpackChunkfront"]=self["webpackChunkfront"]||[]).push([[546],{546:function(e,n,t){t.r(n),t.d(n,{allDay:function(){return l},createEvent:function(){return m},dateFormat:function(){return v},day:function(){return d},default:function(){return y},deleteEvent:function(){return M},month:function(){return i},months:function(){return r},noEvent:function(){return s},today:function(){return f},week:function(){return c},weekDays:function(){return o},year:function(){return u},years:function(){return a}});
-/**
-  * vue-cal v4.8.1
-  * (c) 2022 Antoni Andre <antoniandre.web@gmail.com>
-  * @license MIT
-  */
-const o=["Lunedì","Martedì","Mercoledì","Giovedì","Venerdì","Sabato","Domenica"],r=["Gennaio","Febbraio","Marzo","Aprile","Maggio","Giugno","Luglio","Agosto","Settembre","Ottobre","Novembre","Dicembre"],a="Anni",u="Anno",i="Mese",c="Settimana",d="Giorno",f="Oggi",s="Nessun evento",l="Tutto il giorno",M="Cancella",m="Crea evento",v="dddd D MMMM YYYY",y={weekDays:o,months:r,years:"Anni",year:"Anno",month:"Mese",week:"Settimana",day:"Giorno",today:"Oggi",noEvent:"Nessun evento",allDay:"Tutto il giorno",deleteEvent:"Cancella",createEvent:"Crea evento",dateFormat:"dddd D MMMM YYYY"}}}]);
-//# sourceMappingURL=546.92d3d860.js.map
