017635a5ecb3d86d1a31208fe854fb93c4a69cd2
Donghoon Kim <magenta1223@naver.com>
2022-12-21 01:19:33 +0900

    update backend url

--- config/front/src/views/Auth/RegisterUser.vue
+++ config/front/src/views/Auth/RegisterUser.vue
@@ -67,8 +67,8 @@ import axios from "axios";
 
 
 
-// let url = "http://127.0.0.1:8000/api/register/";
-let url = "http://0.0.0.0:8000/api/register/";
+let url = "http://127.0.0.1:8000/api/register/";
+// let url = "http://0.0.0.0:8000/api/register/";
 
 
 
